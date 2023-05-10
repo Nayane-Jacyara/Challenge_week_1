21b82047706bf58eb8b82d576e73a018b1427799
Nayane-Jacyara <njcs@discente.ifpe.edu.br>
2023-05-10 00:32:19 -0300

    Completed question 1, about validating whether a certain employee is entitled to a bonus

--- question1/Employee.java
+++ question1/Employee.java
@@ -4,10 +4,16 @@ public class Employee {
     
     String name;
     double salary;
+    double bonus;
+    double discount;
+    double bonusOrDiscount;
+    double liquidSalary;
 
-    public Employee(String name, double salary) {
+    public Employee(String name, double salary,double discount) {
         this.name = name;
         this.salary = salary;
+        this.bonus = 0;
+        this.discount = 0;
     }
 
     public String getName() {
@@ -25,5 +31,41 @@ public class Employee {
     public void setSalary(double salary) {
         this.salary = salary;
     }
-}
+    
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+
+    public void calculate() {
+        double bonusOrDiscount = 0;
+
+    if (salary <= 1000) {
+        bonusOrDiscount = salary * 0.2;
+        System.out.println("Bonus:" + bonusOrDiscount);
+    } else if (salary <= 2000) {
+        bonusOrDiscount = salary * 0.1;
+        System.out.println("Bonus:" + bonusOrDiscount);
+    } else {
+        bonusOrDiscount = -salary * 0.1;
+        System.out.println("Discount:" + bonusOrDiscount);
+    }
+    liquidSalary = salary + bonusOrDiscount;
+    System.out.println("Liquid Salary:" + liquidSalary);
+    this.bonusOrDiscount = bonusOrDiscount;
+    }
+
+}       
 
--- question1/Validate.java
+++ question1/Validate.java
@@ -5,26 +5,26 @@ public class Validate {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite o número de funcionários a cadastrar: ");
+        System.out.print("Enter the number of employees to register: ");
         int numEmployees = scanner.nextInt();
 
-        Employee[] employees = new  Employee[numEmployees];
+        Employee[] employees = new Employee[numEmployees];
 
         for (int i = 0; i < numEmployees; i++) {
-            System.out.print("Digite o nome do funcionário " + (i + 1) + ": ");
+            System.out.print("Enter the employee name " + (i + 1) + " : ");
             String name = scanner.next();
-
-            System.out.print("Digite o salário do funcionário " + (i + 1) + ": ");
+ 
+            System.out.print("Enter the employee salary " + (i + 1)+ " : ");
             double salary = scanner.nextDouble();
 
-            employees[i] = new  Employee(name, salary);
+            employees[i] = new Employee(name,salary,i);
         }
 
-        System.out.println("\nFuncionários cadastrados:");
-
-        for (int i = 0; i <numEmployees; i++) {
-            Employee Employee  = employees[i];
-            System.out.println(Employee.getName() + " - R$" + Employee.getSalary());
+        for (int i = 0; i < numEmployees; i++) {
+            Employee employee = employees[i];
+            System.out.println( "Employee: " + employee.getName() + " \nSalary: R$" + employee.getSalary());
+            employee.calculate();
         }
-    }
+        
+    }                
 }
